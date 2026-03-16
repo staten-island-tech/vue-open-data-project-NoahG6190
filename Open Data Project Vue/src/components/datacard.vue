485cdb2e740ec6f5dd48f9b5a4b39f659182ec16
NoahG6190 <noahg258@nycstudents.net>
2026-03-16 14:01:28 -0400

    new code 3/16

---
+++ Open Data Project Vue/src/components/datacard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card">
+        <h2>{{ data.name }}</h2>
+        <p>{{ data.description }}</p>
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    data: {
+        type: Object,
+        required: true
+    }
+});
+</script>
+
+<style scoped>
+
+</style>
