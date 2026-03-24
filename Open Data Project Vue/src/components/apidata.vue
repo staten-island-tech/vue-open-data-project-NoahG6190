21711fea976ced1b8d75d833dab4f7ea499c9cc6
NoahG6190 <noahg258@nycstudents.net>
2026-03-24 13:22:03 -0400

    Update apidata.vue

--- Open Data Project Vue/src/components/apidata.vue
+++ Open Data Project Vue/src/components/apidata.vue
@@ -1,13 +1,16 @@
 <template>
     <div class="container">
- <Datacard v-for "(items, i) in data"
- :key= "item.unique_squirrel_id"
- :age="item.age || ''"
-      :primaryfurcolor="item.primary_fur_color || ''"
-      :x="Number(item.x) || 0"
-      :y="Number(item.y) || 0"
-    />
-    </div>
+
+    <Datacard
+      v-for="(item, i) in data"
+  :key="item.unique_squirrel_id"
+  :uniquesquirrelid="item.unique_squirrel_id"
+  :age="item.age || ''"
+  :primaryfurcolor="item.primary_fur_color || ''"
+  :x="Number(item.x) || 0"
+  :y="Number(item.y) || 0"
+/>
+  </div>
 </template>
 
 <script setup >
@@ -17,13 +20,15 @@ import Datacard from './datacard.vue';
 const data = ref([]);
 
 async function fetchData() {
-    try {
-        const response = await fetch("https://data.cityofnewyork.us/api/v3/resource vfnx-vebw/query.json");
-       const json = await response.json();
-       data.value = json;
-    } catch (error) {
-        console.error( error);
-    }
+  try {
+const response = await fetch(
+  "https://data.cityofnewyork.us/resource/uvpi-gqnh.json"
+)
+     const json = await response.json();
+     data.value = Array.isArray(json) ? json : (json.data || json.results || []);
+  } catch (error) {
+    console.error(error);
+  }
 }
 onMounted(() => {
     fetchData();
